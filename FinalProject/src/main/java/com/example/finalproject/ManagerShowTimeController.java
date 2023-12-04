0dcb475d38b4927a3a2dbbadb722035c9ad65816
JLac <le2056237"crcmail.com>
2023-12-04 10:28:49 -0500

    Show Time display start

--- FinalProject/src/main/java/com/example/finalproject/ManagerShowTimeController.java
+++ FinalProject/src/main/java/com/example/finalproject/ManagerShowTimeController.java
@@ -1,4 +1,48 @@
 package com.example.finalproject;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ManagerShowTimeController {
+    /**
+     * ListView holding the list of show times to be displayed.
+     */
+    @FXML
+    private ListView<String> aListView;
+    /**
+     * MovieList holding the actual show times.
+     */
+    private ShowTimeList showTimeList;
+    /**
+     * Constructor setting the showtime list to be displayed.
+     */
+    public ManagerShowTimeController() {
+        this.showTimeList = ShowTimeList.getInstance();
+    }
+    /**
+     * Loading the list for the first time after setting up.
+     */
+    public void initialize() {
+        this.updateList();
+    }
+    /**
+     * Loading or updating the ListView from showtime list.
+     */
+    public void updateList() {
+        this.aListView.getItems().setAll(showTimeList.getAllShowTimes());
+    }
+
+    /**
+     * Executed on back button click. Going back without saving.
+     * @param pEvent Triggered event. Not used, but necessary for JavaFX.
+     */
+    @FXML
+    protected void backButtonClick(ActionEvent pEvent) {
+        Stage currentStage = (Stage) this.aListView.getScene().getWindow();
+        currentStage.close();
+    }
 }
--- FinalProject/src/main/java/com/example/finalproject/ShowTimeList.java
+++ FinalProject/src/main/java/com/example/finalproject/ShowTimeList.java
@@ -11,16 +11,16 @@ public class ShowTimeList {
     /**
      * list of showtimes
      */
-    private List<ShowTime> showTimes = new LinkedList<ShowTime>();
+    private List<ShowTime> aShowTimes = new LinkedList<ShowTime>();
 
     private static ShowTimeList instance;
     /**
      * Constructs a new list with default samples
      */
     public ShowTimeList(){
-        this.showTimes.add(new ShowTime("Dry paint", 67, 1, new Date()));
-        this.showTimes.add(new ShowTime("Fast and Furious 55", 55, 2, new Date()));
-        this.showTimes.add(new ShowTime("Cabin in the Woods 2", 39, 3, new Date()));
+        this.aShowTimes.add(new ShowTime("Dry paint", 67, 1, new Date()));
+        this.aShowTimes.add(new ShowTime("Fast and Furious 55", 55, 2, new Date()));
+        this.aShowTimes.add(new ShowTime("Cabin in the Woods 2", 39, 3, new Date()));
     }
 
     public static ShowTimeList getInstance(){
@@ -37,7 +37,14 @@ public class ShowTimeList {
      * @return
      */
     public boolean addShowTime(ShowTime pShowTime) {
-        return this.showTimes.add(pShowTime);
+        return this.aShowTimes.add(pShowTime);
     }
 
+    public LinkedList<String> getAllShowTimes() {
+        LinkedList<String> result = new LinkedList<>();
+        for (ShowTime showTime : aShowTimes) {
+            result.add(showTime.toString());
+        }
+        return result;
+    }
 }
